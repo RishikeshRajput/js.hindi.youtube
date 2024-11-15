bf4e1d005168543bf78d766fb6d1fa064217e64c
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-15 11:06:47 +0000

    config done

--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -1,1 +1,17 @@
-const accountId = 1234
+const accountId = 1234 // const is used for declaring constants //
+let accountEmail = "rishi@123" // let used to declare variables //
+var accountPassword = "123" // var is also used for declaring variables , not in practice //
+accountCity = "Burhapur" // do not declare variable like this , without assigning the type //
+
+let accountState // semi colon ';' is not necessary to use // 
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope  
+*/
+
+console.log(accountId)
+
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
