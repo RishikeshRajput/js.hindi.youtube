6c543fe0cee008d6f8b498cffffe2187d359a336
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-15 11:40:25 +0000

    config done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all js code as newer version //
+
+//alert(3+3) we are using nodejs,  not browser 
+
+console.log(3
+    +
+    3)  // this way code can be run but not good practice , code readability should be high //
+
+console.log("radhe radhe")
+
+
+let name = "rishi"
+let age = 27
+let isLoggedIn = false
+let state = null
+
+
+/* primitive datatypes
+
+number=> 2 to power 53
+bigint
+string=> ""
+boolean=> true/false
+null=> standalone value
+undefined=> 
+symbol=> unique
+   
+
+object 
+
+*/
+
+
+// note : typeof=> used for knowing the type of data eg....
+ console.log(typeof age)
+
+ console.log(typeof null) // typeof null is object  
+
+ 
+ console.log(typeof undefined) // typeof undefined is undefined 
