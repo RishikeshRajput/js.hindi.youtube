1c59520e31bcee84c96b5337b57641de81ddf4e6
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-15 14:35:40 +0000

    config done

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -17,3 +17,34 @@ let value = null
 let valInNumber = Number(value)
 console.log(valInNumber) 
 console.log(typeof valInNumber)
+
+
+let nul = undefined
+let nulInNumber = Number(undefined)
+console.log(nulInNumber) 
+console.log(typeof nulInNumber)
+
+
+let tru = true
+let truInNumber = Number(true)
+console.log(truInNumber) 
+console.log(typeof truInNumber)
+
+
+
+//conversion kaise hoga..number ka
+// "33"=> 33
+// "33abc"=> NaN
+// true=> 1, false=> 0  etc..
+
+
+// conversion kaise hoga boolean ka..
+//1=> true, 0=> false
+// "" => false ("" empty string)
+// "rishi"=> true
+
+let someNumber = 33
+let stringNum = String(someNumber)
+
+console.log(stringNum)
+console.log(typeof stringNum)
