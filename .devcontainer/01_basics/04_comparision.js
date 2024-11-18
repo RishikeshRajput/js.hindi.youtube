a7287a65eb01f5cabadb361269335d09a0123bc9
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-18 10:52:46 +0000

    config done

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,35 @@
+
+
+// Note: comparision is done when the datatypes are same 
+
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// Note: comparision done when datatyoe diff. becoz it covert the datatype
+
+// console.log("2"<1)
+// console.log("02">1)
+
+// Note:  comparision " >=,<=,>,<" and equality check "=="" are two different things in js
+// {  avoid this following kind of conversion this will lead you to confusion
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// }
+
+
+// Strict Check : "===" triple equals it checks not only values but datatypes as well
+
+
+// console.log("2" === 2);
+
