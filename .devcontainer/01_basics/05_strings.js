070787aa8aefd5a39fa7beb239cd514525b7f02a
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-22 11:35:40 +0000

    config done

--- .devcontainer/01_basics/05_strings.js
+++ .devcontainer/01_basics/05_strings.js
@@ -12,19 +12,31 @@
 
 // Strings methods or functions
 
-const gameName = new String('Mario Forever') // another way of declaring a string
+//const gameName = new String('Mario Forever') // another way of declaring a string
 
-console.log(gameName[0] ); // op: M, index value or key value pair in js eg. o:M, 1:a, 2:r etc
-console.log(gameName.__proto__);
+// console.log(gameName[0] ); // op: M, index value or key value pair in js eg. o:M, 1:a, 2:r etc
+// console.log(gameName.__proto__);
 
-console.log(gameName.length); // op: 13
-console.log(gameName.toUpperCase()); // op: MARIO FOREVER
-console.log(gameName.charAt(2)); // op: r
-console.log(gameName.indexOf('i')); //op: 3
-console.log(gameName.substring(0, 4)); // op: Mari , we cannot pass -ve values in substring
+// console.log(gameName.length); // op: 13
+// console.log(gameName.toUpperCase()); // op: MARIO FOREVER
+// console.log(gameName.charAt(2)); // op: r
+// console.log(gameName.indexOf('i')); //op: 3
+// console.log(gameName.substring(0, 4)); // op: Mari , we cannot pass -ve values in substring
 
-const newString = gameName.slice(-13, -4)// in slice we can excess -ve index of chars
-console.log(newString);// op: Mario for
+// const newString = gameName.slice(-13, -4)// in slice we can excess -ve index of chars
+// console.log(newString);// op: Mario for
 
 
+// const newStr = "   Rishi   "
+// console.log(newStr);
+// console.log(newStr.trim()); // 'trim()' removes the extra spaces in the string
+
+
+ //const url = "https://github.com/Rishikesh%20Rajput/js.hindi.youtube"
+// console.log(url.replace('%20', '_'));// 'replace('what to replace', 'with what you replace')' will replace the values inside string
+//console.log(url.includes('Rajput')); // 'includes('keyword'), will check if the keyword is present in the strings and gives boolean values as a result
+//console.log(url.includes('prajakta'));
+
+// const newStr2 = "rishi-rajput-analyst"
+// console.log(newStr2.split('-'));
 
