bd9b02df29658af40534706bdf1b98418301c423
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-25 14:31:08 +0000

    config done

--- .devcontainer/01_basics/07_datesInJs.js
+++ .devcontainer/01_basics/07_datesInJs.js
@@ -17,10 +17,22 @@ let myDate = new Date()
 // console.log(myCreatedDate.toLocaleString()); // op: 1/23/2023, 5:03:00 AM
 
 // let myCreatedDate = new Date("2023, 01, 26") // op: 1/26/2023, 12:00:00 AM , "yyyy, mm, dd"
-let myCreatedDate = new Date("01, 26, 2023") // op: 1/26/2023, 12:00:00 AM, "mm, dd, yyyy"
+//let myCreatedDate = new Date("01, 26, 2023") // op: 1/26/2023, 12:00:00 AM, "mm, dd, yyyy"
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now() // we can declare as 'new Date' as well
-console.log(myTimeStamp); // op: 1732460301069 , time comes in miliseconds
-console.log(myCreatedDate.getTime()); // op: 1674691200000, time in milisec acc. to this date
+// let myTimeStamp = Date.now() // we can declare as 'new Date' as well
+// console.log(myTimeStamp); // op: 1732460301069 , time comes in miliseconds
+// console.log(myCreatedDate.getTime()); // op: 1674691200000, time in milisec acc. to this date
 
+// console.log(Date.now()); // op: 1732542428854, gives you op in miliseconds
+// console.log(Math.floor(Date.now()/1000)); // op: 1732542811, gives you op in seconds while div by 1000, math.floor removes decimal val
+
+ let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+ 1); // +1 becoz month start from 0 in js.
+// console.log(newDate.getDay());
+
+// note : customization can be done in dateand time , mostly used is 'toLocaleString'
+// console.log(newDate.toLocaleString('default',{
+//     weekday : "long"
+// }))
