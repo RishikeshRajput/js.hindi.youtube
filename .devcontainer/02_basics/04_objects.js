7454205bd5f90fa65f3dc42d687195c15a3cba32
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-27 14:49:28 +0000

    config done

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -26,6 +26,40 @@ const regularUser = {
 }
 
 
-console.log(regularUser.fullName.userFullName.firstName); // op: Rishikesh
-console.log(regularUser.fullName);
+//console.log(regularUser.fullName?.userFullName.firstName); // op: Rishikesh
+// note : the "?" question mark, after onj name is given in for in case if you are not sure about if
+// that particular object is present or not
+
+//console.log(regularUser.fullName); // op: { userFullName: { firstName: 'Rishikesh', lastName: 'Rajput' } } 
+
+
+
+
+//**********************************imp8***************
+// Merging objects:
+// syntax : const returnedTarget = Object.assign(target, source);
+// note: acc. to syntax the source will merge into target but if we want that all obj merge into new obj we give 
+// empty braces {}, as a target
+
+//const target = { a: 1, b: 2 };
+//const source = { b: 4, c: 5 };
+
+//const returnedTarget = Object.assign(target, source);
+
+//console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+//console.log(returnedTarget === target);
+// Expected output: true
+
+// note eg.
+
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "c", 4 : "d"}
+const obj3 = {5 : "e", 6 : "f"}
+
+const newObj = Object.assign({}, obj1, obj2, obj3)
+// giving empty braces is not necessary but is a safe practice 
+console.log(newObj); // op: { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
 
