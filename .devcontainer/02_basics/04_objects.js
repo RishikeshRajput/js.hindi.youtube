f4d49cb2a4b60f95de28eefcb6c80991ae7dcb3a
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-11-28 11:20:10 +0000

    config done

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -58,8 +58,51 @@ const obj1 = {1 : "a", 2 : "b"}
 const obj2 = {3 : "c", 4 : "d"}
 const obj3 = {5 : "e", 6 : "f"}
 
-const newObj = Object.assign({}, obj1, obj2, obj3)
+//const newObj = Object.assign({}, obj1, obj2, obj3)
 // giving empty braces is not necessary but is a safe practice 
-console.log(newObj); // op: { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+//console.log(newObj); // op: { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+// another way to merge objects is by using "spread" eg.
+
+// const newObj1 = {...obj1, ...obj2, ...obj3}
+// console.log(newObj1);// op: { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+
+// how to access objects when it comes from the database in the form of array
+
+const users = [
+    {
+        id : 1,
+        email : "r@gmail.com"
+    },
+    {
+        id : 2,
+        email : "p@gmail.com"
+    },
+    {
+        id : 3,
+        email : "d@gmail.com"
+    }
+]
+
+// console.log(users[0].email); // op: r@gmail.com
+// console.log(users[1].id); // op: 2
+
+// to perform operation on objects we convert them into key or val and store them in an array, 
+// so we can iterate through them...eg
+
+console.log(tinderUser1); //op : { id: '123abc', name: 'sammy', isloggedIn: false }
+
+console.log(Object.keys(tinderUser1)); // op: [ 'id', 'name', 'isloggedIn' ]
+console.log(Object.values(tinderUser1)); // op: [ '123abc', 'sammy', false ]
+
+// we use "entries" to convert key val in an array, gives us an array inside an array
+console.log(Object.entries(tinderUser1)); // op: [ [ 'id', '123abc' ], [ 'name', 'sammy' ], [ 'isloggedIn', false ] ]
+
+// to check if the property is present in the obj or not
+console.log(tinderUser1.hasOwnProperty('isloggedIn')); // op : true
+console.log(tinderUser1.hasOwnProperty('islogged')); // op: false
+
+
 
 
