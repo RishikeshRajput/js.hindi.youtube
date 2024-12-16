2577816f684a6592189b9249029646f878369041
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-12-16 14:40:35 +0000

    config done

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,65 @@
+// Switch :- syntax..
+
+// switch (key) {  *** key is the condi. which we have to check..
+//     case value:      *** value is the condi. which we comapre to the key ...
+        
+//         break;
+
+//     default:       *** if no condi. is matched than default case will be excecuted
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+//         break;
+//     case 3:
+//         console.log("mar");
+        
+//         break;
+//     case 4:
+//         console.log("apr");
+        
+//         break;
+
+//     default:
+//         console.log("no case match");
+        
+//         break;
+// } //op : mar
+
+// Note: if we won't use break statement than all the cases will be excecuted after the condi. satisfied , except the "default statement"..
+
+const month = "march"
+
+switch (month) {
+    case "january":
+        console.log("jan");
+        
+        break;
+    case "february":
+        console.log("feb");
+        
+        break;
+    case "march":
+        console.log("mar");
+        
+        break;
+    case "april":
+        console.log("apr");
+        
+        break;
+
+    default:
+        console.log("no case match");
+        
+        break;
+} // op : mar
+
