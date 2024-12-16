7772763c83c1888377cb3433c07fbb20c6d58886
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-12-16 14:16:34 +0000

    config done

--- .devcontainer/04_control_flow/one.js
+++ .devcontainer/04_control_flow/one.js
@@ -41,3 +41,71 @@ const score = 200
 // console.log(`User power : ${power}`); // op: ReferenceError: power is not defined, becoz it is out of 
 //scope
 
+
+// ############## Implicit scope ###############
+
+//const balance = 1000
+
+//if (balance > 500) console.log("test"); // op :test
+
+// we can write multiple lines in a implicit scope by using coma, but at end we need to use semi colon.....
+
+//if (balance > 500) console.log("test") , console.log("test2");
+// op : test
+//      test2
+// note: this type of code very unreadble code, so it's not a good practice, should not write like this..
+
+// Nesting ###############
+
+// if (balance < 500){
+//     console.log("less than 500");
+
+// } else if (balance < 700){
+//     console.log("less than 750");
+
+// } else if (balance < 900){
+//     console.log("less than 900");
+
+// } else {
+//     console.log("less than 1200");
+    
+
+// } // op: less than 1200
+
+
+// ##### && ###### this ampersant operator will check if both the conditions are to or not..
+
+const userLoggedIn = true
+const debitCard = true
+
+// if (userLoggedIn && debitCard){
+//     console.log("allow to buy course");
+    
+// } else{
+//     console.log("don't allow");
+    
+// } // op: allow to buy course
+
+
+
+// we can give multiple conditions as well but all should be true to excecute code eg. ...
+
+// if (userLoggedIn && debitCard && 2==2){
+//     console.log("allow to buy course");
+    
+// } else{
+//     console.log("don't allow");
+    
+// } // op: allow to buy course
+
+
+
+// ######## || ##### this pipe operator used as "or", in which one of the condition should be true..
+
+// const loggedInFromGoogle = true
+// const loggedInFromEmail = true
+
+// if (loggedInFromEmail || loggedInFromGoogle || true){
+//     console.log("allow to logIn");
+    
+// } // op : allow to logIn
