1c23bfe3064e5f0a5133cfecf20dbdcd5be8019d
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-12-16 11:07:59 +0000

    config done

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,32 @@
+// if :
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const isLoggedIn = true
+
+// if ( 3 == "3"){
+//     console.log("executed");
+    
+// } // op : executed
+
+// if ( 3 === "3"){
+//     console.log("executed");
+    
+// } // no op. bcoz its strict equal or triple equal(===) condi. which also type check.
+
+
+// if ( 2 !== "2"){
+//     console.log("executed");
+    
+// } // op : executed, becoz the type is different
+
+
+const temperature = 41
+
+// if (temperature == 51){
+//     console.log("less than 50");
+// } else {
+//     console.log("greater than 50");
+// }
+
+// console.log("executed");
