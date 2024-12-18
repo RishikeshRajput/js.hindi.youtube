918cc6bfcf0ededadec3037d159e59f1f83329d4
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-12-18 10:42:41 +0000

    config done

---
+++ .devcontainer/04_control_flow/truthy.js
@@ -0,0 +1,37 @@
+const userEmail = []
+
+// if (userEmail){
+//     console.log("user got email");
+// }
+// else {
+//     console.log("dont have email");
+    
+// }
+// op: user got email, because square brackets are considers as truthy val..
+
+// Falsy values:
+
+// false, 0, -0, NaN, null, undefined, "", BigInt 0n
+// other than this all are truthy values eg...
+
+// Truthy values:
+
+// "0", "false", " ", [], {}, function(){}
+
+// To check if the array is empty, we have a method...
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+// }
+
+
+// To check if the object is empty, we have a method...
+
+const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty");
+    
+// }
+
