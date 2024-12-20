4dfd1a090bc6bd40cc530e864e79faa39a34a5d2
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-12-20 10:43:17 +0000

    config done

--- .devcontainer/04_control_flow/truthy.js
+++ .devcontainer/04_control_flow/truthy.js
@@ -35,3 +35,32 @@ const emptyObj = {}
     
 // }
 
+// ############# Nullish Coalescing Operator (??) : null, undefined #########################
+
+// it works on null and undefined, it basically does the typecheck of the value assign to the variable, if the 
+// value is null or undefined or in some cases two values will be return at same time where we use "nullish coalescing op. (??)"
+
+let val1 ; 
+
+// val1 = 5 ?? 10 // op: 5, it took the random val
+
+//val1 = null ?? 10 // op: 10, it took 10 becoz other val is null
+
+//val1 = undefined ?? 15 // op: 15, it took 10 becoz other val is undefined
+
+//val1 = undefined ?? 25 ?? 20 // op : 25, in case of more than two val where one in null or undefined , it will always take the first val
+
+//console.log(val1);
+
+// note: (??) is also used to handle errors
+
+
+
+// ############# Terniary Operator (?) ############## 
+
+// Syntax : condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("greater than 80");
+
