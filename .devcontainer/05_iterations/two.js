fdf0a42ce116609cac3ff510e559e716d4ff9151
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-12-20 14:29:43 +0000

    config done

---
+++ .devcontainer/05_iterations/two.js
@@ -0,0 +1,41 @@
+// While loop:....
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`val of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// let myArray = ["rishi", "pankya", "baghel", "panda"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`name is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// dowhile loop:....
+
+// let score = 1
+
+// do {
+//     console.log(`val is ${score}`);
+//     score ++
+// } while (score <= 5); 
+//op:-
+// val is 1
+// val is 2
+// val is 3
+// val is 4
+// val is 5
+
+let score = 5
+
+do {
+    console.log(`val is ${score}`);
+    score ++
+} while (score <= 5);  
+// op :val is 5, becoz it will do the operation first than check the condition
+// we rarely use dowhile loop in the condi. where atleast once have to iterate the loop...
+
