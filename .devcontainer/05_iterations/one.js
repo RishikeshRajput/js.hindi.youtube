0f48daf3a1e66222feb8575c04d7084fe76b6c5b
RishikeshRajput <117081453+RishikeshRajput@users.noreply.github.com>
2024-12-20 11:39:42 +0000

    config done

---
+++ .devcontainer/05_iterations/one.js
@@ -0,0 +1,64 @@
+// for loop:...
+
+// for syntax : 
+// for (let i = 0; i < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);    
+// } 
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is a good no.");
+        
+//     }
+//     console.log(element);    
+// } 
+
+
+// for (let i = 0; i < 3; i++) {
+//     const element1 = i;
+//     console.log(element1);
+    
+//     for (let j = 0; j < 3; j++) {
+//         const element2 = j;
+//         console.log(element2);
+        
+//     }
+// }
+
+const myArray = ["rishi", "pankya", "baghel"]
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+    
+// }
+
+
+// for (let index = 1; index <= 10; index++) {
+//     if (index == 5) {
+//         console.log("5 detected");
+//         break
+//     }
+//     console.log(`value of index is ${index}`);
+    
+// } // break will end the the loop
+
+
+
+// for (let index = 1; index <= 10; index++) {
+//     if (index == 5) {
+//         console.log("5 detected");
+//         continue
+//     }
+//     console.log(`value of index is ${index}`);
+    
+// }// continue will skip that iteration
